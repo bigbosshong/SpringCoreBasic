ab73b7f7ca25a8ebd853e16eb65ca3da650456d1
bigbosshong1101 <bigbosshong1101@gmail.com>
2023-10-02 20:00:54 +0900

    관심사의 분리 (Dependency Injection)

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -2,11 +2,12 @@ package hello.core;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
-import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemberService;
 
 public class MemberApp {
     public static void main(String[] args) {
-        MemberServiceImpl memberService = new MemberServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
         Member member = new Member(1L, "memberA", Grade.Vip);
         memberService.join(member);
 
